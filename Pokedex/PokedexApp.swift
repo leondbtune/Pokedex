0e8e37577b4b08abdf71e0095da883bb76c4890b
leondbtune <leon.bethuyne@student.hogent.be>
2024-08-16 22:00:56 +0200

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Leon Bethuyne on 16/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
